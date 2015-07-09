24b39c994597a1cd055bd261cd588694af8a4bfd
Julio Melo <juliocsmelo@gmail.com>
2015-07-09 20:47:39 -0300

    add express for create the wedserver

---
+++ appgen/requestHandler.js
@@ -0,0 +1,7 @@
+function hello(response) {
+  console.log("Request handler 'hello' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("hello world");
+  response.end();
+}
+exports.hello = hello;
--- appgen/server.js
+++ appgen/server.js
@@ -2,55 +2,72 @@ var fs = require('fs')
     , path = require('path')
     , _ = require('underscore')
     , fse = require('fs.extra')
-    , futil = require('./fileUtil.js');
+    , http = require('http')
+    , futil = require('./fileUtil.js')
+    , url = require('url')
+    , express = require('express');
 
+
+var app = express();
 var targetPath = "./target/";
 var srcPath = "./template/";
 var jsonFile = "./site1.json";
 
-run_cmd("rm", ["-rf", targetPath], function() {});
-run_cmd("mkdir", [targetPath], function() {});
-var siteData = createSiteData(jsonFile);
-console.log("Copying files...");
-fse.copyRecursive(srcPath, targetPath, function(err) {
-  if (! err) {
-    futil.transform(siteData, srcPath, targetPath);
-    futil.createStaticFiles(siteData, targetPath + "www/contents/templates/", "./contentTemplate/");
-  }
+
+// Creating a Ionic app from github template
+// run_cmd("", ["-rf", targetPath], function() {});
+//
+// run_cmd("", ["-rf", targetPath], function() {});
+// run_cmd("mkdir", [targetPath], function() {});
+// var siteData = createSiteData(jsonFile);
+// console.log("Copying files...");
+// fse.copyRecursive(srcPath, targetPath, function(err) {
+//   if (! err) {
+//     futil.transform(siteData, srcPath, targetPath);
+//     futil.createStaticFiles(siteData, targetPath + "www/contents/templates/", "./contentTemplate/");
+//   }
+// });
+//
+//
+// function createSiteData(jsonFile) {
+//   var siteData = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
+//   var pageMap = {}
+//   _.each(siteData.pages, function(page) {
+//     pageMap[page.id] = page;
+//   })
+//   siteData.pageMap = pageMap;
+//
+//   menuPages = [];
+//   _.each(siteData.menus, function(pageId) {
+//     menuPages.push(pageMap[pageId]);
+//   });
+//
+//   siteData['menuPages'] = menuPages;
+//
+//   return siteData;
+// }
+//
+// function run_cmd(cmd, args, callBack ) {
+//   var spawn = require('child_process').spawn;
+//   var child = spawn(cmd, args);
+//   var resp = "";
+//
+//   child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+//   child.stdout.on('end', function() { callBack (resp) });
+// }
+//
+//
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
+app.post('/', function (req, res) {
+  res.send('POST! Build the app');
+});
 
-function createSiteData(jsonFile) {
-  var siteData = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
-  var pageMap = {}
-  _.each(siteData.pages, function(page) {
-    pageMap[page.id] = page;
-  })
-  siteData.pageMap = pageMap;
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-  menuPages = [];
-  _.each(siteData.menus, function(pageId) {
-    menuPages.push(pageMap[pageId]);
-  });
-
-  siteData['menuPages'] = menuPages;
-
-  return siteData;
-}
-
-function run_cmd(cmd, args, callBack ) {
-  var spawn = require('child_process').spawn;
-  var child = spawn(cmd, args);
-  var resp = "";
-
-  child.stdout.on('data', function (buffer) { resp += buffer.toString() });
-  child.stdout.on('end', function() { callBack (resp) });
-}
-
-
-
-
-
-
-
-
+  console.log('Example app listening at http://%s:%s', host, port);
+});

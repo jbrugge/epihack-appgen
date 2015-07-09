777cbf9108bbb0e59feab772ab9ea54b353f9525
John Brugge <john.brugge@gmail.com>
2015-07-08 23:37:38 -0300

    Rename to be more descriptive

--- appgen/generate_apk.js
+++ appgen/generate_apk.js
@@ -3,15 +3,15 @@ var fs = require('fs');
 var appConfigFile = process.argv[2];
 var appInfo = JSON.parse(fs.readFileSync(appConfigFile, 'utf8'));
 
-function run_cmd(cmd, args, options, callback) {
+function run_cmd_sync(cmd, args, options, callback) {
   var spawn = require('child_process').spawnSync,
     child = spawn(cmd, args, options),
     resp = child.stdout.toString();
 	callback(resp);
 }
 
-cwd = "";
-run_cmd('pwd', [], {}, function(resp) { cwd = resp.toString() });
+var cwd = "";
+run_cmd_sync('pwd', [], {}, function(resp) { cwd = resp.toString().trim() });
 
 var targetDir = cwd + "/target",
 	apkDir = targetDir + "/platforms/android/build/outputs/apk/",
@@ -24,11 +24,11 @@ var targetDir = cwd + "/target",
 //ANDROID_BUILD_TOOLS_DIR = process.env.ANDROID_HOME + "/build_tools/22.0.1/"
 ANDROID_BUILD_TOOLS_DIR = "/home/vagrant/android-sdk-linux/build-tools/22.0.1/"
 
-run_cmd('cordova', ['build', '--release', 'android'], 
+run_cmd_sync('cordova', ['build', '--release', 'android'], 
 	{cwd: targetDir}, function(resp) { console.log(resp) });
-run_cmd('keytool', ['-genkey', '-noprompt', '-keystore', keyStore, '-keypass', keyPass, '-storepass', keyPass, '-alias', appInfo.appName, '-dname', keyDname, '-keyalg', 'RSA', '-keysize', '2048', '-validity', '10000'], 
+run_cmd_sync('keytool', ['-genkey', '-noprompt', '-keystore', keyStore, '-keypass', keyPass, '-storepass', keyPass, '-alias', appInfo.appName, '-dname', keyDname, '-keyalg', 'RSA', '-keysize', '2048', '-validity', '10000'], 
 	{cwd: targetDir}, function(resp) { console.log(resp) });
-run_cmd('jarsigner', ['-sigalg', 'SHA1withRSA', '-digestalg', 'SHA1', '-keystore', keyStore, '-keypass', keyPass, '-storepass', keyPass, unsignedApk, appInfo.appName], 
+run_cmd_sync('jarsigner', ['-sigalg', 'SHA1withRSA', '-digestalg', 'SHA1', '-keystore', keyStore, '-keypass', keyPass, '-storepass', keyPass, unsignedApk, appInfo.appName], 
 	{cwd: apkDir}, function(resp) { console.log(resp) });
-run_cmd(ANDROID_BUILD_TOOLS_DIR + 'zipalign', ['-v', '4', unsignedApk, signedApk], 
+run_cmd_sync(ANDROID_BUILD_TOOLS_DIR + 'zipalign', ['-v', '4', unsignedApk, signedApk], 
 	{cwd: apkDir}, function(resp) { console.log(resp) });

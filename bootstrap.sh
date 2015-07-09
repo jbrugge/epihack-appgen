31ff8b4662e4dee4fac4d75800a3c86b583fec19
John Brugge <john.brugge@gmail.com>
2015-07-08 23:20:17 -0300

    Use the right sdk version.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,6 @@
 ANDROID_SDK_FILENAME=android-sdk_r24-linux.tgz
 ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 
-#sudo apt-get install python-software-properties
-#sudo add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install -y npm git openjdk-7-jdk ant expect
 npm install -g n
@@ -22,11 +20,13 @@ npm install -g cordova
 npm install -g ionic
 expect -c '
 set timeout -1   ;
-spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-19,build-tools-19.1.0
+spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-22,build-tools-22.0.1
 expect { 
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
 }
 '
+# Fix the newly updated SDK directories
+sudo chown -R vagrant android-sdk-linux/
 
 sudo gem install sass

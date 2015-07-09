27a43f729009bbbab14e2e6826fd28a669113b6c
Julio Melo <juliocsmelo@gmail.com>
2015-07-09 20:58:10 -0300

    webserver

--- appgen/server.js
+++ appgen/server.js
@@ -14,54 +14,54 @@ var srcPath = "./template/";
 var jsonFile = "./site1.json";
 
 
-// Creating a Ionic app from github template
-// run_cmd("", ["-rf", targetPath], function() {});
-//
-// run_cmd("", ["-rf", targetPath], function() {});
-// run_cmd("mkdir", [targetPath], function() {});
-// var siteData = createSiteData(jsonFile);
-// console.log("Copying files...");
-// fse.copyRecursive(srcPath, targetPath, function(err) {
-//   if (! err) {
-//     futil.transform(siteData, srcPath, targetPath);
-//     futil.createStaticFiles(siteData, targetPath + "www/contents/templates/", "./contentTemplate/");
-//   }
-// });
-//
-//
-// function createSiteData(jsonFile) {
-//   var siteData = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
-//   var pageMap = {}
-//   _.each(siteData.pages, function(page) {
-//     pageMap[page.id] = page;
-//   })
-//   siteData.pageMap = pageMap;
-//
-//   menuPages = [];
-//   _.each(siteData.menus, function(pageId) {
-//     menuPages.push(pageMap[pageId]);
-//   });
-//
-//   siteData['menuPages'] = menuPages;
-//
-//   return siteData;
-// }
-//
-// function run_cmd(cmd, args, callBack ) {
-//   var spawn = require('child_process').spawn;
-//   var child = spawn(cmd, args);
-//   var resp = "";
-//
-//   child.stdout.on('data', function (buffer) { resp += buffer.toString() });
-//   child.stdout.on('end', function() { callBack (resp) });
-// }
-//
-//
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.post('/', function (req, res) {
+  // Creating a Ionic app from github template
+  run_cmd("", ["-rf", targetPath], function() {});
+
+  run_cmd("", ["-rf", targetPath], function() {});
+  run_cmd("mkdir", [targetPath], function() {});
+  var siteData = createSiteData(jsonFile);
+  console.log("Copying files...");
+  fse.copyRecursive(srcPath, targetPath, function(err) {
+    if (! err) {
+      futil.transform(siteData, srcPath, targetPath);
+      futil.createStaticFiles(siteData, targetPath + "www/contents/templates/", "./contentTemplate/");
+    }
+  });
+
+
+  function createSiteData(jsonFile) {
+    var siteData = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
+    var pageMap = {}
+    _.each(siteData.pages, function(page) {
+      pageMap[page.id] = page;
+    })
+    siteData.pageMap = pageMap;
+
+    menuPages = [];
+    _.each(siteData.menus, function(pageId) {
+      menuPages.push(pageMap[pageId]);
+    });
+
+    siteData['menuPages'] = menuPages;
+
+    return siteData;
+  }
+
+  function run_cmd(cmd, args, callBack ) {
+    var spawn = require('child_process').spawn;
+    var child = spawn(cmd, args);
+    var resp = "";
+
+    child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+    child.stdout.on('end', function() { callBack (resp) });
+  }
   res.send('POST! Build the app');
 });
 

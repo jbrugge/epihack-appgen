ffcb9b104d297974ec5e0f0c3acd6bcda75843ad
Julio Melo <juliocsmelo@gmail.com>
2015-07-10 00:34:40 -0300

    Add email job and build private key

--- appgen/build.sh
+++ appgen/build.sh
@@ -6,13 +6,13 @@ BUILD_PATH='./builds/'
 JSON_PATH='./www/'
 GIT=$(which ionic)
 LOG_FILE='build.log'
-APP_NAME=$(date +%d%m%Y_%S)
+APP_NAME=$1-$(date +%d%m%Y_%S)
 BASE_URL='http://url_to_api/endpoint/'
 echo 'Starting the build\n' > $LOG_FILE
 
 # TODO
 # + get json files of app skeleton with curl
-# + send e-mail with attach apk to publishing .apk
+# + to use keytool to generate our private key
 if [ ! -d $BUILD_PATH  ]; then
   mkdir $BUILD_PATH
 fi
@@ -27,6 +27,9 @@ if [ -x $(which ionic) ]; then
   echo "Creating app from apihack-ionoic repository\n" >> $LOG_FILE
   ionic start $APP_NAME https://github.com/opendream/epihack-ionoic
   cordova build --release android
+  # keytool -genkey -noprompt -keystore keyStore -keypass keyPass -storepass keyPass -alias $APP_NAME -dname keyDname, '-keyalg', 'RSA', '-keysize', '2048', '-validity', '10000']
+  jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore appgenerator.keystore $APP_NAME-release-unsigned.apk appgenerator
+  zipalign -v 4 $APP_NAME-release-unsigned.apk $APP_NAME.apk
   echo "Done! App created\n" >> $LOG_FILE
   exit 0
 else
